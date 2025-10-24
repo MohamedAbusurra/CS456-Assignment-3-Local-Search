ed05a443e5bed17f31bf55c5781f5b97b11daf6e
mohabel <mohab1077@gmail.com>
2025-10-24 15:52:04 +0300

    added p,E to SA solve

--- SimulatedAnnealingAlgorithm.java
+++ SimulatedAnnealingAlgorithm.java
@@ -15,13 +15,30 @@ class SimulatedAnnealingAlgorithm{
    }
 
    public void solve(){
-    int t = 100;
+    double t = 100;
     int h_old = h(queen);
+    String text = queen;
+    char[] arr = text.toCharArray();
     while(true){
         Random random = new Random(seed);
         Random random2 = new Random(seed);
         int col = random.nextInt(8);
         int row = random2.nextInt(8);
+        t = t*0.95;
+        arr[col] = (char)(row + '0');
+        text = new String(arr);
+        int h_new = h(text);
+        int E = h_new - h_old;
+        if(E>0){
+            double x = (-E)/t;
+            double p = Math.exp(x);
+            Random random3 = new Random(seed);
+            double u = random3.nextDouble();
+            if(!(u>p)){
+                h_old = h_new;
+            }
+
+        }
 
     }
    }
