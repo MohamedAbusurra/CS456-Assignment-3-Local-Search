40cb1164e5937a6eeeb726d99fd170f28f45f076
mohabel <mohab1077@gmail.com>
2025-10-24 13:44:44 +0300

    added the structure of SA

---
+++ SimulatedAnnealingAlgorithm.java
@@ -0,0 +1,11 @@
+class SimulatedAnnealingAlgorithm{
+    private String queen;
+    private int seed;
+
+    public SimulatedAnnealingAlgorithm(String queen,int seed){
+        this.queen = queen;
+        this.seed = seed;
+    }
+
+   
+}
