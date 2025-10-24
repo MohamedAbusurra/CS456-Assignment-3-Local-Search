6c4148e748dfb5cdaa85ddc477450b4cbaf2843c
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-10-24 16:36:23 +0200

    feat: add input state reader for 8x8 queen configuration
    - Reads a string for reading the input starting state for the 8x8 queen configuration
    
    - converts string to integers and stores them in an array of integers for easier use while performing algortihms

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Enter starting queen position configuration: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String[] queenInputConfiguration = scanner.nextLine().split(" ");
+        int[] queenPositionArray = new int[queenInputConfiguration.length];
+
+        for (int i = 0; i < queenInputConfiguration.length; i++) {
+            queenPositionArray[i] = Integer.parseInt(queenInputConfiguration[i]);
+            System.out.println(queenPositionArray[i]);
+        }
 
     }
 }
