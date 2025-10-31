d33a34d54c6459aab16b7cd1337a9151377be15d
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-10-31 19:43:15 +0200

    feat: implement constructor to open csv file in class

--- CsvResults.java
+++ CsvResults.java
@@ -1,10 +1,16 @@
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class CsvResults {
 
     private FileWriter writer;
 
     private CsvResults(String fileName) {
+        try {
+            writer = new FileWriter(fileName);
+        } catch (IOException e) {
+            System.err.println("Error opening csv file!");
+        }
 
     }
 
